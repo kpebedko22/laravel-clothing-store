d3b54451a8b8ff7aad0b9995b59de429877d7820
Alexander <alex-78910@mail.ru>
2024-01-06 16:10:31 +0700

    Clear code

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ClothingStoreController;
 use App\Http\Controllers\Web\IndexController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,11 +15,12 @@ Route::name('web.')
         require __DIR__ . '/web/favorite-products.php';
     });
 
-Route::get('/cart', [ClothingStoreController::class, 'cart'])->name('cart');
-
-Route::get('add-item-to-cart-{id}', [ClothingStoreController::class, 'addItemToCart'])->name('cart.add');
-Route::get('delete-from-cart-{id}', [ClothingStoreController::class, 'deleteItemFromCart'])->name('cart.delete');
-Route::post('create-order', [ClothingStoreController::class, 'createOrder'])->name('cart.create');
-
-Route::get('/preview-item-{id}', [ClothingStoreController::class, 'previewItem']);
-Route::get('/preview-cart', [ClothingStoreController::class, 'previewCart']);
+// TODO: remove old routes
+//Route::get('/cart', [ClothingStoreController::class, 'cart'])->name('cart');
+//
+//Route::get('add-item-to-cart-{id}', [ClothingStoreController::class, 'addItemToCart'])->name('cart.add');
+//Route::get('delete-from-cart-{id}', [ClothingStoreController::class, 'deleteItemFromCart'])->name('cart.delete');
+//Route::post('create-order', [ClothingStoreController::class, 'createOrder'])->name('cart.create');
+//
+//Route::get('/preview-item-{id}', [ClothingStoreController::class, 'previewItem']);
+//Route::get('/preview-cart', [ClothingStoreController::class, 'previewCart']);
